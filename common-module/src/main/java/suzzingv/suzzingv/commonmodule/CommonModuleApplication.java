42abbb6ac2c031bfecf44288793bbae1a6d8cafb
suzzingv <sjlim1999@naver.com>
2025-02-24 16:54:25 +0900

    [CI] common-module 변경 테스트

--- common-module/src/main/java/suzzingv/suzzingv/commonmodule/CommonModuleApplication.java
+++ common-module/src/main/java/suzzingv/suzzingv/commonmodule/CommonModuleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommonModuleApplication {
 
+    // common 변경
     public static void main(String[] args) {
         SpringApplication.run(CommonModuleApplication.class, args);
     }
