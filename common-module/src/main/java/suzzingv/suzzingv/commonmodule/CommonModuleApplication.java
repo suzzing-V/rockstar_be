617a9614e08bae782f4c48b90cb0539ee738b386
suzzingv <sjlim1999@naver.com>
2025-02-24 20:52:26 +0900

    [CI] 빌드 경로 수정

--- common-module/src/main/java/suzzingv/suzzingv/commonmodule/CommonModuleApplication.java
+++ common-module/src/main/java/suzzingv/suzzingv/commonmodule/CommonModuleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommonModuleApplication {
 
+    // c
     public static void main(String[] args) {
         SpringApplication.run(CommonModuleApplication.class, args);
     }
