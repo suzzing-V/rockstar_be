cfd0bf65a09c26feab20db929bc4a3ee74497a27
suzzingv <sjlim1999@naver.com>
2025-06-20 00:01:07 +0900

    [FEAT] redis 연결 운영환경과 개발환경 분리

--- src/main/java/suzzingv/suzzingv/rtr/global/redis/RedisConfig.java
+++ src/main/java/suzzingv/suzzingv/rtr/global/redis/RedisConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -16,14 +17,56 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Value("${spring.data.redis.cluster.nodes}")
     private String clusterNodes;
 
     @Value("${spring.data.redis.password}")
     private String password;
 
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    @Profile("dev")
+    public RedisConnectionFactory redisConnectionFactoryDev() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    @Profile("dev")
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactoryDev());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    @Profile("dev")
+    public RedisTemplate<String, Integer> integerRedisTemplate() {
+        RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Integer.class));
+        redisTemplate.setConnectionFactory(redisConnectionFactoryDev());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    @Profile("prod")
+    public RedisConnectionFactory redisConnectionFactoryProd() {
         RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(List.of(clusterNodes));
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
             .useSsl() // 반드시 필요
@@ -33,21 +76,23 @@ public class RedisConfig {
 
     // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
+    @Profile("prod")
+    public RedisTemplate<String, String> redisTemplateProd() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactoryProd());
 
         return redisTemplate;
     }
 
     @Bean
-    public RedisTemplate<String, Integer> integerRedisTemplate() {
+    @Profile("prod")
+    public RedisTemplate<String, Integer> integerRedisTemplateProd() {
         RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Integer.class));
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactoryProd());
 
         return redisTemplate;
     }
