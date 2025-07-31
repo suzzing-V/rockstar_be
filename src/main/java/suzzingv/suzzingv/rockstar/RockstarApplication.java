30aaf60120001134c838a9127031406f9ef74f19
suzzing-V <sjlim1999@gmail.com>
2025-07-31 13:31:26 +0900

    [FEAT] 한국 시간대로 설정

--- src/main/java/suzzingv/suzzingv/rockstar/RockstarApplication.java
+++ src/main/java/suzzingv/suzzingv/rockstar/RockstarApplication.java
@@ -1,12 +1,21 @@
 package suzzingv.suzzingv.rockstar;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class RockstarApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RockstarApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("✅ TimeZone set to Asia/Seoul");
     }
 }
