d214e46d21ce777e24facb84d582dc6ce8e4f9f0
suzzing-V <sjlim1999@gmail.com>
2025-08-29 02:39:41 +0900

    [FEAT] 푸시알림 비동기처리

---
+++ src/main/java/suzzingv/suzzingv/rockstar/global/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package suzzingv.suzzingv.rockstar.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "fcmTaskExecutor")
+    public Executor fcmTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("FCM-Async-");
+        executor.initialize();
+        return executor;
+    }
+}
